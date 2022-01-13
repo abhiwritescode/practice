d92cce89d9218a21295e1039c2c130405771c481
abhiwritescode <95482993+abhiwritescode@users.noreply.github.com>
2022-01-13 07:05:03 -0500

    Update binary-tree.java

--- binary-tree.java
+++ binary-tree.java
@@ -1,4 +1,45 @@
 
+//  Insert into a Binary Search Tree
+//You are given the root node of a binary search tree (BST) and a value to insert into the tree. 
+// Return the root node of the BST after the insertion. 
+// It is guaranteed that the new value does not exist in the original BST.
+
+/*
+simplest - always go for leaf O(log n)
+
+while()
+    if val > n.val
+        if ( n.rt != null)
+            n= n.rt
+        else 
+            n.rt = new NOde(val)
+     else
+        n=n.left
+*/
+public TreeNode insertIntoBST(TreeNode root, int val) {
+    
+    Node n = root;
+    
+    while (n != null){
+        if (val > n.value){
+            if (n.right != null)
+                n= n.right;
+            else {
+                n.right = new Node(val);
+                break;
+            }
+        } else {
+             if (n.left != null)
+                n= n.right;
+            else {
+                n.right = new Node(val);
+                break;
+            }
+       }
+    }
+    return root;
+        
+}
 
 // next lower, given node and link to parent
 
